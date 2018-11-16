674450b373552716435a0a276f11df6c550d82d2
Diego Corrêa <diegocorrea.cc@gmail.com>
2018-11-16 18:09:43 -0300

    tfidf

--- main.py
+++ main.py
@@ -5,9 +5,7 @@ from src import preprocessing
 
 # Load dataset
 DATASET = make_dataset()
-print(DATASET.head())
 pp = preprocessing.main_start(DATASET)
 print(pp.head())
 data_df, polarity_class = model_vectorize(pp)
-print(pp.head())
 machine_algorithms.main(data_df, polarity_class)
--- src/model_vec.py
+++ src/model_vec.py
@@ -2,7 +2,7 @@ import string
 import nltk
 import numpy as np
 import pandas as pd
-from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
 
 
 def __lem_tokens(tokens):
@@ -25,15 +25,15 @@ def __lem_normalize(text):
 def tf_as_matrix(sentence_list):
     LemVectorizer = CountVectorizer(tokenizer=__lem_normalize, stop_words='english')
     LemVectorizer.fit_transform(sentence_list)
-    # print(LemVectorizer.vocabulary_)
-    # print(sorted(LemVectorizer.vocabulary_.items(), key=lambda kv: kv[1])[:50])
-    words_couted = LemVectorizer.vocabulary_.items()
+    word_position = LemVectorizer.vocabulary_.items()
     tf_matrix = LemVectorizer.transform(sentence_list).toarray()
-    # print(tf_matrix)
-    return tf_matrix, words_couted
+    tfidfTran = TfidfTransformer(norm="l2")
+    tfidfTran.fit(tf_matrix)
+    tfidf_matrix = tfidfTran.transform(tf_matrix)
+    return tfidf_matrix.toarray(), sorted(word_position , key=lambda k: (k[1], k[0]))
 
 
 def model_vectorize(dataset_df):
-    tf_matrix, words_couted = tf_as_matrix(sentence_list=dataset_df['stem_sentence'].tolist())
-    data_df = pd.DataFrame(data=np.matrix(tf_matrix), columns=[a for a, v in words_couted])
+    tfidf_matrix, word_position = tf_as_matrix(sentence_list=dataset_df['stem_sentence'].tolist())
+    data_df = pd.DataFrame(data=np.matrix(tfidf_matrix), columns=[a for a, v in word_position])
     return data_df, dataset_df['polarity']
