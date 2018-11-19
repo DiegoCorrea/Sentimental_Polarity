d4055cf5c28e1cf075a6e559638bcdd31550f880
Diego Corrêa <diegocorrea.cc@gmail.com>
2018-11-19 17:02:56 -0300

    colocando ciclos

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from src.mining_data import make_dataset
 from src.model_vec import model_vectorize
 from src import machine_algorithms
 from src import preprocessing
+import pandas as pd
 
 
 if __name__ == '__main__':
@@ -14,8 +15,11 @@ if __name__ == '__main__':
     DATASET = preprocessing.main_start(DATASET)
     DATASET.info(memory_usage='deep')
     print("\n")
-    print("3.\tPreparando os modelos")
-    data_df, polarity_class = model_vectorize(DATASET)
-    print("\n")
-    print("4.\tAprendizado")
-    print(machine_algorithms.main(data_df, polarity_class))
+    results_df = pd.DataFrame(data=[], columns=['round', 'model', 'algorithm', 'precision', 'mae'])
+    for i in range(5):
+        print("3.\tPreparando os modelos")
+        data_df, polarity_class = model_vectorize(DATASET)
+        print("\n")
+        print("4.\tAprendizado")
+        results_df = pd.concat([results_df, machine_algorithms.main(data_df, polarity_class, i, 'Model_1')], sort=False)
+    print(results_df)
--- src/machine_algorithms.py
+++ src/machine_algorithms.py
@@ -2,10 +2,11 @@ import numpy as np
 from sklearn import metrics
 from sklearn import tree
 from sklearn.linear_model import Perceptron
-from sklearn.metrics import confusion_matrix, precision_score
+from sklearn.metrics import precision_score, mean_absolute_error
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.naive_bayes import GaussianNB
+import pandas as pd
 
 
 def train_naive_bayes(x_train, y_train):
@@ -37,34 +38,44 @@ def split_data(data_df, polarity_class):
 
 
 def evaluate(y_pred, y_test):
-    return dict({
-        'MAE': metrics.mean_absolute_error(y_test, y_pred),
-        'Precision': precision_score(y_test, y_pred)
-    })
+    return mean_absolute_error(y_test, y_pred), precision_score(y_test, y_pred)
 
 
-def main(data_df, polarity_class):
+def main(data_df, polarity_class, run, model):
     evaluate_results_as_dict = dict()
+    result_df = pd.DataFrame(data=[], columns=['round', 'model', 'algorithm', 'precision', 'mae'])
     # Divisão dos dados em treinamento e teste
     x_train, x_test, y_train, y_test = split_data(data_df, polarity_class)
     # Uso dos dados no treinamento e teste do Perceptron RNA, por fim avaliação dos resultados
     print("\tPerceptron")
     clf = train_perceptron(x_train, y_train)
     y_pred = clf.predict(x_test)
-    evaluate_results_as_dict['Perceptron'] = evaluate(y_pred, y_test)
+    mae, precision = evaluate(y_pred, y_test)
+    result_df = pd.concat([result_df, pd.DataFrame(data=[[run, model, 'Perceptron', precision, mae]],
+                                                   columns=['round', 'model', 'algorithm', 'precision', 'mae'])],
+                          sort=False)
     # Uso dos dados no treinamento e teste da Árvore de Decisão, por fim avaliação dos resultados
     print("\tÁrvore de Decisão")
     clf = train_tree(x_train, y_train)
     y_pred = clf.predict(x_test)
-    evaluate_results_as_dict['AD'] = evaluate(y_pred, y_test)
+    mae, precision = evaluate(y_pred, y_test)
+    result_df = pd.concat([result_df, pd.DataFrame(data=[[run, model, 'AD', precision, mae]],
+                                                   columns=['round', 'model', 'algorithm', 'precision', 'mae'])],
+                          sort=False)
     # Uso dos dados no treinamento e teste do KNN, por fim avaliação dos resultados
     print("\tKNN")
     clf = train_knn(x_train, y_train)
     y_pred = clf.predict(x_test)
-    evaluate_results_as_dict['KNN'] = evaluate(y_pred, y_test)
+    mae, precision = evaluate(y_pred, y_test)
+    result_df = pd.concat([result_df, pd.DataFrame(data=[[run, model, 'KNN', precision, mae]],
+                                                   columns=['round', 'model', 'algorithm', 'precision', 'mae'])],
+                          sort=False)
     # Uso dos dados no treinamento e teste do Naive Bayes, por fim avaliação dos resultados
     print("\tNaive Bayes")
     clf = train_naive_bayes(x_train, y_train)
     y_pred = clf.predict(x_test)
-    evaluate_results_as_dict['NB'] = evaluate(y_pred, y_test)
-    return evaluate_results_as_dict
+    mae, precision = evaluate(y_pred, y_test)
+    result_df = pd.concat([result_df, pd.DataFrame(data=[[run, model, 'NB', precision, mae]],
+                                                   columns=['round', 'model', 'algorithm', 'precision', 'mae'])],
+                          sort=False)
+    return result_df
